ae23eff4913d279e23ae248bb008097ecac21051
jadson <jadson.g-matos@outlook.com>
2022-09-23 21:46:21 -0300

    api

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,87 @@
 use std::thread;
 
-use std::io::{Read, Seek, BufReader, BufRead};
+use std::env;
 use std::fs::File;
 use std::io::SeekFrom;
-use std::env;
+use std::io::{stdout, BufRead, BufReader, Read, Seek, Write};
+
+use curl::easy::Easy;
 
 fn main() {
     let num_cpus = thread::available_parallelism().unwrap().get();
-    let args: Vec<String> = env::args().collect();
-    println!("{} {:?}", num_cpus, args);
-
-    if args.len() == 4 {
-        let mut file;
-        match File::open(&args[3]) {
-            Ok(s) => {
-                file = s;
-            }
-            Err(e) => {
-                println!("File open {}", e);
-                return;
-            }
-        }
-
-        //let mut reader = BufReader::with_capacity(num_cpus*(21*1024),file);
-
-        let mut buf = [0; 21];
-        let mut i: u64 = args[1].parse().unwrap();
-        loop {
-            file.seek(SeekFrom::Start(i)).expect("seek failed");
-
-            match file.read_exact(&mut buf) {
-                Ok(_) => {}
-                Err(e) => {
-                    println!("File read {}", e);
-                    break;
-                }
-            }
-
-            let check = check_palindrome(&buf);
-            if check == 0 {
-                let palavra = std::str::from_utf8(&buf).unwrap();
-                println!("palindrome: ${} #{:?} {:?}", i, thread::current().id(), &palavra);
-                if rust_math::num::is_prime(palavra.parse().unwrap()) {
-                    println!("palindrome primo: ${} #{:?} {:?}", i, thread::current().id(), &palavra);
-                    break;
-                }
-                i = i + 1;
-            } else {
-                i = i + check as u64;
-            }
+    println!("{}", num_cpus);
 
 
-            if i % 100000 == 0 {
-                println!("${} {:?}", i, std::str::from_utf8(&buf));
-            }
 
-            if i == args[2].parse().unwrap(){
+    //let mut reader = BufReader::with_capacity(num_cpus*(21*1024),file);
+    let mut easy = Easy::new();
+    let mut buf = [0; 21];
+    let mut i: u64 = 0;
+
+    loop {
+
+        // Write the contents of rust-lang.org to stdout
+        
+        easy.url(format!("https://api.pi.delivery/v1/pi?start={i}&numberOfDigits=1000&radix=10").as_str()).unwrap();
+        easy.write_function(|data| {
+            println!("data: {:?}",data);
+            Ok(data.len())
+        }).unwrap();
+
+        /* 
+        if check_palindrome(&buf) {
+            let palavra = std::str::from_utf8(&buf).unwrap();
+
+            println!(
+                "palindrome: ${} #{:?} {:?}",
+                i,
+                thread::current().id(),
+                &palavra
+            );
+            let num = u128::from_str_radix(palavra, 10).unwrap();
+            if prime(&num) {
+                println!(
+                    "palindrome primo: ${} #{:?} {:?}",
+                    i,
+                    thread::current().id(),
+                    &palavra
+                );
                 break;
             }
         }
+
+        i = i + 1000;
+
+        if i % 100000 == 0 {
+            println!("${} {:?}", i, std::str::from_utf8(&buf));
+        }
+        */
     }
 }
 
+fn prime(y: &u128) -> bool {
+    let mut aux = 2;
+    let mut div = 0;
 
-fn check_palindrome(palavra: &[u8; 21]) -> u8 {
+    loop {
+        if aux <= y / 2 {
+            if y % aux == 0 {
+                div = div + 1;
+            }
+        } else {
+            break;
+        };
+        aux = aux + 1;
+    }
+
+    if div == 0 {
+        return true;
+    }
+
+    false
+}
+
+fn check_palindrome(palavra: &[u8; 21]) -> bool {
     if palavra[0] == palavra[20] {
         if palavra[1] == palavra[19] {
             if palavra[2] == palavra[18] {
@@ -74,7 +92,7 @@ fn check_palindrome(palavra: &[u8; 21]) -> u8 {
                                 if palavra[7] == palavra[13] {
                                     if palavra[8] == palavra[12] {
                                         if palavra[9] == palavra[11] {
-                                            return 0;
+                                            return true;
                                         }
                                     }
                                 }
@@ -84,97 +102,7 @@ fn check_palindrome(palavra: &[u8; 21]) -> u8 {
                 }
             }
         }
-    } else if palavra[2] == palavra[20] {
-        if palavra[3] == palavra[19] {
-            if palavra[4] == palavra[18] {
-                if palavra[5] == palavra[17] {
-                    if palavra[6] == palavra[16] {
-                        if palavra[7] == palavra[15] {
-                            if palavra[8] == palavra[14] {
-                                if palavra[9] == palavra[13] {
-                                    if palavra[10] == palavra[12] {
-                                        return 1;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } else if palavra[4] == palavra[20] {
-        if palavra[5] == palavra[19] {
-            if palavra[6] == palavra[18] {
-                if palavra[7] == palavra[17] {
-                    if palavra[8] == palavra[16] {
-                        if palavra[9] == palavra[15] {
-                            if palavra[10] == palavra[14] {
-                                if palavra[11] == palavra[13] {
-                                    return 2;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } else if palavra[6] == palavra[20] {
-        if palavra[7] == palavra[19] {
-            if palavra[8] == palavra[18] {
-                if palavra[9] == palavra[17] {
-                    if palavra[10] == palavra[16] {
-                        if palavra[11] == palavra[15] {
-                            if palavra[12] == palavra[14] {
-                                return 3;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    } else if palavra[8] == palavra[20] {
-        if palavra[9] == palavra[19] {
-            if palavra[10] == palavra[18] {
-                if palavra[11] == palavra[17] {
-                    if palavra[12] == palavra[16] {
-                        if palavra[13] == palavra[15] {
-                            return 4;
-                        }
-                    }
-                }
-            }
-        }
-    } else if palavra[10] == palavra[20] {
-        if palavra[11] == palavra[19] {
-            if palavra[12] == palavra[18] {
-                if palavra[13] == palavra[17] {
-                    if palavra[14] == palavra[16] {
-                        return 5;
-                    }
-                }
-            }
-        }
-    } else if palavra[12] == palavra[20] {
-        if palavra[13] == palavra[19] {
-            if palavra[14] == palavra[18] {
-                if palavra[15] == palavra[17] {
-                    return 6;
-                }
-            }
-        }
-    } else if palavra[14] == palavra[20] {
-        if palavra[15] == palavra[19] {
-            if palavra[16] == palavra[18] {
-                return 7;
-            }
-        }
-    } else if palavra[16] == palavra[20] {
-        if palavra[17] == palavra[19] {
-            return 8;
-        }
-    } else if palavra[18] == palavra[20] {
-        return 9;
-    }
+    } 
 
-    return 10;
+    false
 }
